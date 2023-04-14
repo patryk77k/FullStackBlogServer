a46c6287a828400d39d287c1a98bb1e9588da1bc
patryk77k <patryk77.k@gmail.com>
2023-04-14 20:26:00 +0200

    password compare work

--- index.js
+++ index.js
@@ -27,4 +27,10 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username: username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  res.json(passOk);
+});
 app.listen(4000);
