a1edf8a1d435634c89f52bacd03c3abbf8b65d0a
patryk77k <patryk77.k@gmail.com>
2023-04-14 21:22:09 +0200

    register and login to home page fixed

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 const salt = bcrypt.genSaltSync(10);
-app.use(cors());
+const secret = "qdwqdwefqef";
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 
 mongoose.connect(
@@ -29,8 +32,16 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const userDoc = await User.findOne({ username: username });
+  const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
-  res.json(passOk);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("wrong credentials");
+  }
 });
 app.listen(4000);
