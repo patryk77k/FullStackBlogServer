d7feff09033e121b6431325fbe730ebf4213160d
patryk77k <patryk77.k@gmail.com>
2023-04-14 18:22:54 +0200

    register fixed

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const bcrypt = require("bcryptjs");
 const app = express();
 
+const salt = bcrypt.genSaltSync(10);
 app.use(cors());
 app.use(express.json());
 
@@ -13,11 +15,16 @@ mongoose.connect(
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const userDoc = await User.create({
-    username,
-    password,
-  });
-  res.json(userDoc);
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
 });
 
 app.listen(4000);
