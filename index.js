d1774a949371b7f0157ee1e96bc0d2defcaf2bf1
patryk77k <patryk77.k@gmail.com>
2023-04-18 00:48:36 +0200

    end commit

--- index.js
+++ index.js
@@ -89,6 +89,37 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   });
 });
 
+////PUT
+
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    const { id, title, summary, content } = req.body;
+    if (err) throw err;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("you are not the author");
+    }
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+    res.json(postDoc);
+  });
+});
+
 app.get("/post", async (req, res) => {
   res.json(
     await Post.find()
@@ -98,4 +129,11 @@ app.get("/post", async (req, res) => {
   );
 });
 
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
+
 app.listen(4000);
+//
